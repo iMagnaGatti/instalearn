583b58a96f6d04b307b9af0328c0f47a6174c843
Alberto Vendramini <alberto.vendra@gmail.com>
2022-10-02 22:15:36 +0200

    lol

---
+++ Client/scripts/main.js
@@ -0,0 +1,24 @@
+function start()
+{
+    document.getElementById("prova").innerHTML="prova";
+}
+function signup()
+{
+    window.location.href="./signup.html";
+}
+function login()
+{
+    window.location.href="./login.html";
+}
+function insegnante()
+{
+    window.location.href="./insegnante.html";
+}
+function test()
+{
+    window.location.href="./test.html";
+}
+function user()
+{
+    window.location.href="./user.html";
+}
