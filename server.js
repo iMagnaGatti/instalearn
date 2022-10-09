de1892024bf463d4da1a005717e0f3004968396a
Alberto Vendramini <alberto.vendra@gmail.com>
2022-10-09 18:56:25 +0200

    limite richieste

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
 if(process.env.NODE_ENV !=='production')
 {
-    require('dotenv').config()
+    require('dotenv').config();
 }
 
-const express= require('express')
-const app=express()
-const mongoose = require('mongoose')
-const bodyParser=require('body-parser')
-const bcrypt=require('bcrypt')
+const express= require('express');
+const app=express();
+const mongoose = require('mongoose');
+const bodyParser=require('body-parser');
 var CryptoJS = require("crypto-js");
+const cors = require('cors');
+var corsOptions = {
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {;
+        callback(new Error('Not allowed by CORS'));
+      }
+    }
+  }
+const helmet = require('helmet');
+const sanitizer = require('sanitizer');
+const rateLimit = require("express-rate-limit");
+const port = process.env.PORT || 3000;
 var ObjectId=mongoose.Types.ObjectId;
-const SimpleCrypto=require('simple-crypto-js').default
 const _secretKey = process.env.SECRET_KEY; //key for create hash key 
-const simpleCrypto = new SimpleCrypto(_secretKey);
-const saltRounds=12
-var createHashPwd = function (password) {
-    return bcrypt.hashSync(password, saltRounds);
-  };
-const urlencodedParser = bodyParser.urlencoded({ extended: true })
+const urlencodedParser = bodyParser.urlencoded({ extended: true });
+mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true});
+const db=mongoose.connection;
 
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser: true})
-const db=mongoose.connection
+
 db.on('error',error=>console.error(error))
 db.once('open',()=>console.error('Connected to mongoose'))
 app.use(function (req, res, next) {
@@ -41,6 +50,16 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+app.use(helmet());
+app.use(express.static('.'));
+//app.use(cors(corsOptions));
+app.use(rateLimit({windowMs: 10 * 60 * 1000, max: 100}));
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+})); 
+
+
+
 app.post('/signup',express.json(),async (req,res)=>{
     console.log(req.body);
     const nome=req.body.nome;
@@ -110,4 +129,9 @@ app.post('/cercaInsegnante',express.json(),async (req,res)=>{
     res.sendStatus(500);
 
 });
-app.listen(process.env.PORT||3000)
+
+
+
+
+
+app.listen(port)
