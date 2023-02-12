fec8640d7eedc956d11e97362a04e08c0b31f4b6
Armando <armypellegrini@gmail.com>
2023-02-12 20:18:25 +0100

    singup test

--- test/singup.test.js
+++ test/singup.test.js
@@ -2,128 +2,43 @@ import { createMocks } from "node-mocks-http";
 import signup from "server.js/signup";
 
 describe("POST server.js/signup", () => {
-  it("should return 500, no password", async () => {
+  it("should return 500, missing data", async () => {
     const { req, res } = createMocks({ method: "POST" });
     await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password is essential!",
-    });
+    expect(res.statusCode).toBe(400);
   });
-  it("should return 500, password too short", async () => {
+  it("should return 400, email already exist", async () => {
     const { req, res } = createMocks({
       method: "POST",
-      body: { password: "prova" },
+      body: { 
+        Nome: "armando" ,
+        Cognome: "Pellegrini",
+        Username: "armypelle",
+        Password: "Password_123",
+        Descrizione: "prova",
+        Email: "armypelle69@gmail.com"
+      
+      },
     });
     await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password must be at least 8 characters",
-    });
-  });
-  it("should return 500, password too long", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "f".repeat(257) },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password too long! (max: 256)",
-    });
-  });
-  it("should return 500, password must contain uppercase", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "foobarbaz" },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password must contain at least 1 uppercase letter",
-    });
-  });
-  it("should return 500, password must contain symbol", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "Foobarbaz" },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password must contain at least 1 symbol letter",
-    });
-  });
-  it("should return 500, password must contain lowercase", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "FOOBARBAZ" },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password must contain at least 1 lowercase letter",
-    });
-  });
-  it("should return 500, password must contain number", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "Foobarbaz." },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Password must contain at least 1 number letter",
-    });
-  });
-  it("should return 500, email required", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "Foobarbaz.1" },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Email is required!",
-    });
-  });
-  it("should return 500, invalid email", async () => {
-    const { req, res } = createMocks({
-      method: "POST",
-      body: { password: "Foobarbaz.1", email: "foo" },
-    });
-    await signup(req, res);
-    expect(res.statusCode).toBe(500);
-    expect(res._getData()).toEqual({
-      success: false,
-      error: "Invalid email",
-    });
+    expect(res.statusCode).toBe(400);
+
   });
   
-  // it("should return 201, created", async () => {
+  
+  // it("should return 200, created", async () => {
   //   const { req, res } = createMocks({
   //     method: "POST",
-  //     body: {
-  //       password: "Foobarbaz.1",
-  //       email: "foo@bar.baz",
-  //       name: "foo",
-  //       surname: "bar",
-  //       isGestore: false,
-  //     },
+  //      body: { 
+  //      Nome: "armando" ,
+  //      Cognome: "Pellegrini",
+  //      Username: "armypelle",
+  //      Password: "Password_123",
+  //      Descrizione: "prova",
+  //      Email: "armypelle96@gmail.com"
+  //    },
   //   });
   //   await signup(req, res);
-  //   expect(res.statusCode).toBe(500);
-  //   expect(res._getData()).toEqual({
-  //     success: true,
-  //     isUnitn: false,
-  //   });
+  //   expect(res.statusCode).toBe(200);
   // });
 });
